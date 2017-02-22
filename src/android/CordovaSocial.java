f127b8f818d07a1b1ac281e1ab48e15585589dea
Gaurav Vachhani <gaurav@theappsmiths.net>
2017-02-22 10:20:25 +0530

    changes in config.xml

--- src/android/CordovaSocial.java
+++ src/android/CordovaSocial.java
@@ -1,11 +1,14 @@
 package org.cordova.social;
 
+import com.facebook.login.LoginManager;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.util.Arrays;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -14,15 +17,16 @@ public class CordovaSocial extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        if (action.equals("loginFB")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.loginFB(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void loginFB(String message, CallbackContext callbackContext) {
+        LoginManager.getInstance().logInWithReadPermissions(cordova.getActivity(), Arrays.asList("email", "public_profile"));
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
